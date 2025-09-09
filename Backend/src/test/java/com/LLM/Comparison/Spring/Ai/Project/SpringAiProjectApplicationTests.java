46811139806319fc8c7c54981d0616fe08ea182d
Ali Sheikh <l_lawliet786@hotmail.com>
2025-09-08 21:53:25 -0400

    initial commit

---
+++ Backend/src/test/java/com/LLM/Comparison/Spring/Ai/Project/SpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LLM.Comparison.Spring.Ai.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
